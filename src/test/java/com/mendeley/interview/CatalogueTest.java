0409e495367e1b1baf473fb883d8ec2aa23a7919
Evgeny Pretko <epretko@efinancialcareers.com>
2022-08-10 11:12:21 +0200

    add catalogue tests

---
+++ src/test/java/com/mendeley/interview/CatalogueTest.java
@@ -0,0 +1,118 @@
+package com.mendeley.interview;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CatalogueTest {
+
+    Catalogue catalogue;
+
+    @BeforeEach
+    void setUp() {
+        catalogue = new Catalogue();
+    }
+
+
+    //  1.	write a test to verify that the Catalog is empty on creation.
+    @Test
+    void testCatalogIsEmptyOnCreation() {
+
+        assertEquals(0, catalogue.size());
+    }
+
+
+    //  2.	add a Document and write a test to verify the Catalog is not empty.
+    @Test
+    void testCatalogIsNotEmptyAfterAddingDocuments() {
+        Document document = new Document("Passport", 2016);
+        catalogue.addDocument(document);
+
+        assertEquals(1, catalogue.size());
+    }
+
+
+    //  3.	write a test to verify that retrieving documents returns the added documents.
+    @Test
+    void testGetDocumentsByYearReturnsAddedDocuments() {
+        Document document = new Document("Passport", 2016);
+        catalogue.addDocument(document);
+
+        List<Document> returnedDocuments = catalogue.getDocumentsByYear(2016);
+
+        assertAll(
+                () -> assertSame(document, returnedDocuments.get(0)),
+                () -> assertEquals("Passport", returnedDocuments.get(0).getTitle()),
+                () -> assertEquals(2016, returnedDocuments.get(0).getPublicationYear()),
+                () -> assertEquals(1, returnedDocuments.size())
+        );
+    }
+
+
+    //  4.	identify and write at least 2 other possible tests
+    @Test
+    void testGetDocumentsByYearReturnsOnlyDocumentsWithProvidedYear() {
+        Document document1 = new Document("Passport", 2016);
+        Document document2 = new Document("Visa", 2022);
+        Document document3 = new Document("Driver License", 2022);
+        catalogue.addDocument(document1);
+        catalogue.addDocument(document2);
+        catalogue.addDocument(document3);
+
+        List<Document> returnedDocuments = catalogue.getDocumentsByYear(2022);
+
+        assertAll(
+                () -> assertFalse(returnedDocuments.contains(document1)),
+                () -> assertTrue(returnedDocuments.containsAll(Arrays.asList(document2, document3)))
+        );
+    }
+
+    @Test
+    void testGetDocumentsByYearReturnsEmptyListIfThereAreNoDocumentsWithProvidedYear() {
+        Document document1 = new Document("Passport", 2016);
+        Document document2 = new Document("Visa", 2022);
+        catalogue.addDocument(document1);
+        catalogue.addDocument(document2);
+
+        List<Document> returnedDocuments = catalogue.getDocumentsByYear(2000);
+
+        assertAll(
+                () -> assertEquals(0, returnedDocuments.size()),
+                () -> assertNotEquals(0, catalogue.size())
+        );
+    }
+
+    @Test
+    void testGetSortedDocumentsReturnsDocumentsSorted() {
+        Document document1 = new Document("Visa", 2022);
+        Document document2 = new Document("Passport", 2016);
+        Document document3 = new Document("Marriage Certificate", 2020);
+        catalogue.addDocument(document1);
+        catalogue.addDocument(document2);
+        catalogue.addDocument(document3);
+
+        List<Document> returnedDocuments = catalogue.getSortedDocuments(new DocumentsComparator());
+
+        assertAll(
+                () -> assertSame(document2, returnedDocuments.get(0)),
+                () -> assertSame(document3, returnedDocuments.get(1)),
+                () -> assertSame(document1, returnedDocuments.get(2))
+        );
+    }
+
+
+    class DocumentsComparator implements Comparator<Document> {
+
+        @Override
+        public int compare(Document doc1, Document doc2) {
+            return doc1.getPublicationYear() - doc2.getPublicationYear();
+        }
+    }
+
+}
